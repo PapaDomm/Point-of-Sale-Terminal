9b9b13acc845e92f35308577bf4f77dc160d6271
Rod Mastria <rodmastria@gmail.com>
2024-02-07 15:28:29 -0500

    Added Product items to main Program

--- Point of Sale/Product.cs
+++ Point of Sale/Product.cs
@@ -1,13 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Point_of_Sale
 {
     internal class Product
     {
         // add properties
+        public string name { get; set; }
+        public string category { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+
+        //constructors
+        public Product() 
+        {
+            name = "GoldenNuggets";
+            category = "jewel";
+            description = "18karat";
+            price = 1000000m;
+        }
+        public Product(
+            string newName, 
+            string newCategory, 
+            string newDescription, 
+            decimal newPrice)
+        {
+            name = newName;
+            category = newCategory;
+            description = newDescription;
+            price = newPrice;
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return $"{name} | {category} | {description} | {price}";
+        }
     }
 }
--- Point of Sale/Program.cs
+++ Point of Sale/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello Group :)");
+﻿// authors: Victor, Dominic, Rod
+// topic: C#.NET Midterm Project
+
+using Point_of_Sale;
+
+Console.WriteLine("--- Welcome to Game Ville! ---");
+
+List<Product> gameProducts = new List<Product>()
+{
+    new Product("Hollow Knight", "Metroidvania", "Fun game about bugs!", 14.99m),
+    new Product("Palworld", "Open World", "GTA Pokemon", 29.99m),
+    new Product("Crash Bandicoot 4", "Platformer", "Jump on boxes and die", 19.99m),
+    new Product("Tekken 8", "3D Fighter", "Throw your son in a volcano", 69.99m),
+    new Product("Persona 3 Reload", "JRPG", "Stay up late after school", 69.99m),
+    new Product("League of Legends", "MOBA", "Torture", 100000m),
+    new Product("Monster Hunter Rise", "JRPG", "Beat up mean animals", 39.99m),
+    new Product("Spiderman Remastered", "Open World", "Vigilante game", 19.99m),
+    new Product("Baldurs Gate 3", "RPG", "Worms in my brain", 59.99m),
+    new Product("Among US", "Social Deduction", "Drama", 20m),
+    new Product("Psychonauts 2", "Platformer", "I miss summer camp", 20.39m),
+    new Product("UNO", "Card Game", "Classic - Test Your Friendships", 9.99m)
+};
+
